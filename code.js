f1ecd9ecf1a721f4f7907cd9911aece54cdb2db7
PA1n7 <64380382+PA1n7@users.noreply.github.com>
2023-03-20 20:07:02 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,35 @@
+var loop = window.setInterval(()=>{
+    currDate = new Date();
+    endDate = new Date("April 3, 2023 04:30");
+
+    sub = new Date(endDate-currDate)
+
+    dateString = (sub.getMonth()+":"+sub.getDate()+":"+sub.getHours()+":"+sub.getMinutes()+":"+sub.getSeconds());
+    document.getElementById("timer").innerText= dateString
+}, 1)
+
+let imgs = document.getElementsByTagName("img")
+
+function clearImg(){
+    for(let i=0; i<imgs.length; i++){
+        imgs[i].style.opacity = "0"
+    }
+}
+
+clearImg()
+imgs[0].style.opacity = "0.5"
+var currPic = 1
+
+var change = window.setInterval(()=>{
+    imgs[currPic].style.opacity = "0.5"
+    let picBefore = currPic-1
+    if (picBefore == -1){
+        picBefore = imgs.length-1
+    }
+    console.log(picBefore)
+    imgs[picBefore].style.opacity = "0"
+    currPic=currPic + 1
+    if (currPic == imgs.length){
+        currPic= 0;
+    }
+}, 2000)
